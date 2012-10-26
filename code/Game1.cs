ef93f493b4a683d92be96b8b33fdea3de7d059cc
ward0248 <ward0248@flinders.edu.au>
2012-10-26 23:54:51 +1030

    added exit using esc key

--- code/Game1.cs
+++ code/Game1.cs
@@ -72,7 +72,7 @@ namespace ForbiddenArtsGame.code
         protected override void Update(GameTime gameTime)
         {
             // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 this.Exit();
 
             // TODO: Add your update logic here
