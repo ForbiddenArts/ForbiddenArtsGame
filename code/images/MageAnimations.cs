27d2a78e81e4eac1898c0cd074327c8f8f34adcd
Miniature <wndavey@hotmail.com>
2012-10-30 19:51:34 +1030

    Added the rest of the mage anitmations

---
+++ code/images/MageAnimations.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace ForbiddenArtsGame.code.images
+{
+	class MageWalk : Animation
+	{
+		public MageWalk()
+			: base()
+		{
+			firstFrameRect = new Rectangle(0, 0, 242, 230);
+			srcRect = new Rectangle(0, 0, 242, 230);
+			origin = new Vector2(121, 115);
+			frameCount = 11;
+			image = SheetHandler.getSheet("characters/Mage");
+		}
+	}
+
+	class MageMelee : Animation
+	{
+		public MageMelee()
+			: base()
+		{
+			firstFrameRect = new Rectangle(0, 230, 242, 230);
+			srcRect = new Rectangle(0, 230, 242, 230);
+			origin = new Vector2(121, 115);
+			frameCount = 9;
+			image = SheetHandler.getSheet("characters/Mage");
+		}
+	}
+	
+	class MageCast : Animation
+	{
+		public MageCast()
+			: base()
+		{
+			firstFrameRect = new Rectangle(0, 460, 242, 230);
+			srcRect = new Rectangle(0, 460, 242, 230);
+			origin = new Vector2(121, 115);
+			frameCount = 8;
+			image = SheetHandler.getSheet("characters/Mage");
+		}
+	}
+
+	class MageIdle : Animation
+	{
+		public MageIdle()
+			: base()
+		{
+			firstFrameRect = new Rectangle(0, 690, 242, 230);
+			srcRect = new Rectangle(0, 690, 242, 230);
+			origin = new Vector2(121, 115);
+			frameCount = 9;
+			image = SheetHandler.getSheet("characters/Mage");
+		}
+	}
+
+	class MageJump : Animation
+	{
+		public MageJump()
+			: base()
+		{
+			firstFrameRect = new Rectangle(0, 920, 242, 230);
+			srcRect = new Rectangle(0, 920, 242, 230);
+			origin = new Vector2(121, 115);
+			frameCount = 9;
+			image = SheetHandler.getSheet("characters/Mage");
+		}
+	}
+
+	class MageFall : Animation
+	{
+		public MageFall()
+			: base()
+		{
+			firstFrameRect = new Rectangle(484, 1150, 242, 230);
+			srcRect = new Rectangle(0, 1150, 242, 230);
+			origin = new Vector2(121, 115);
+			frameCount = 1;
+			currentFrame = -2;
+			image = SheetHandler.getSheet("characters/Mage");
+		}
+	}
+}
