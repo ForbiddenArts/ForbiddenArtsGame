00699338087bba506e9ec0cfa18ee05b99667b26
ward0248 <ward0248@flinders.edu.au>
2012-10-31 14:24:00 +1030

    hopefully fixing spells

---
+++ code/entities/projectiles/spells/Immobolise.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using ForbiddenArtsGame.code.images;
+
+namespace ForbiddenArtsGame.code.entities.projectiles.spells
+{
+	/// <summary>
+	/// Immobolise prevents the character from moving their legs while allowing them to continue attacking.
+	/// Effect is:
+	/// </summary>
+	class Immobolise : Spell
+	{
+		public static bool isAlive;
+		public Immobolise()
+		{
+			name = "Immobolise";
+			currentSprite = new MageCast();
+			//Projectile vars
+			damage = 0;
+			timetolive = 60;
+		}
+
+		public override Spell Cast(Vector2 loc, Vector2 dir, Entity source)
+		{
+			isAlive = true;
+			this.source = source;
+			velocity = 5 * dir;
+			this.loc = new Vector2(loc.X+100,loc.Y);
+			return this;
+		}
+
+		public override void Update(GameTime gameTime)
+		{
+			base.Update(gameTime);
+			if (lifetime >= timetolive)
+			{
+				toBeRemoved.Add(this);
+				isAlive = false;
+			}
+		}
+
+		public override void Collide(Entity e)
+		{
+			Character entity = (Character)e;
+			if (entity != null)
+			{
+				if (e == source)
+					return;
+				e.AddEffect(new ImmoboliseEffect());
+				toBeRemoved.Add(this);
+				isAlive = false;
+			}
+		}
+	}
+
+	class ImmoboliseEffect : SpellEffect
+	{
+		public override void Effect(entities.Entity e)
+		{
+			Character spellHit = (Character)e;
+			if (spellHit != null)
+			{
+				spellHit.Move(new Vector2(1,-100));
+			}
+		}
+	}
+}
---
+++ code/entities/projectiles/spells/`Spell.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using ForbiddenArtsGame.code.entities;
+
+/*
+ * Spell types are stored in a list of spells that a player has and can cast
+ * they are instantiated with Spell.Cast
+ * 
+ * SpellEffects effecting an entity are stored in a list in that entity, and Effect is called each update cycle\
+ *		unless instant = true, then it is called once when the effect starts and the effect isnt stored in the list
+ * 
+ */
+
+namespace ForbiddenArtsGame.code.entities.projectiles.spells
+{
+	abstract class SpellEffect
+	{
+		public bool instant { get; protected set; }
+
+		public SpellEffect()
+		{
+			instant = true;
+		}
+
+		public abstract void Effect(Entity e);
+	}
+
+	abstract class Spell : Projectile
+	{
+		public string name { get; protected set; }
+
+		public Spell()
+		{
+			name = "UNKNOWN SPELL";
+		}
+		public abstract Spell Cast(Vector2 loc, Vector2 dir, Entity source);
+	}
+}
