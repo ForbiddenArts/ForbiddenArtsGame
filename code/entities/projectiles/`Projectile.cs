6caf66918e4e15bccd728b0f28dd310b1eadf269
Miniature <wndavey@hotmail.com>
2012-11-01 02:59:55 +1030

    That's a much better way to do that

--- code/entities/projectiles/`Projectile.cs
+++ code/entities/projectiles/`Projectile.cs
@@ -42,23 +42,13 @@ namespace ForbiddenArtsGame.code.entities.projectiles
 
 		public override void Collide(Entity e)
 		{
-			Character entity;
-			try
+			if (e is Character)
 			{
-				entity = (Character)e;
-			}
-#pragma warning disable 168 //"variable not used" warning
-			catch (InvalidCastException ex)
-#pragma warning restore 168
-			{
-				entity = null;
-			}
-			if (entity != null)
-			{
-				if (e == source)
+				Character c = (Character)e;
+				if (c == source)
 					return;
-				entity.Damage(damage);
-				entity.Move(new Vector2(0, 10));
+				c.Damage(damage);
+				c.Move(new Vector2(0, 10));
 			}
 		}
 	}
