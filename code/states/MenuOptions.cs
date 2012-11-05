3fb1a083fedba5cc2e79391997f552336b708eb6
maho0051 <maho0051@flinders.edu.au>
2012-11-05 11:42:06 +1030

    added keyboard functionality to options menu, but not others

--- code/states/MenuOptions.cs
+++ code/states/MenuOptions.cs
@@ -28,10 +28,13 @@ namespace ForbiddenArtsGame.code.states
 		SpriteFont font;
 		Texture2D Mainmenu;
 		Rectangle Mainrect;
-		enum BindSelected { Left, Right, Jump, Interact, Melee, Spell1, Spell2, Inventory }
+		enum BindSelected { Left, Right, Jump, Interact, Melee, Spell1, Spell2, Inventory, None }
 		BindSelected selectedBind;
+        KeyboardState oldState;
 		bool binding = false;
 		bool NeedMouseLift = true;
+        int selected = 0;
+        int numOptions = 12;
 		public MenuOptions(bool _drawParent)
 			: this()
 		{
@@ -74,6 +77,7 @@ namespace ForbiddenArtsGame.code.states
 			font = SheetHandler.getSpriteFont();
 			Mainmenu = SheetHandler.getSheet("menu/options/opt_main+");
 			Mainrect = new Rectangle(806, 491, 140, 38);
+            oldState = Keyboard.GetState();
 		}
 
 		public override bool Update(GameTime gameTime)
@@ -84,7 +88,7 @@ namespace ForbiddenArtsGame.code.states
 				{
 					binding = false;
 				}
-				else if (Keyboard.GetState().GetPressedKeys().Length > 0)
+				else if (Keyboard.GetState().GetPressedKeys().Length > 0 && oldState.IsKeyUp(Keys.Enter) && binding)
 				{
 					switch (selectedBind)
 					{
@@ -110,8 +114,9 @@ namespace ForbiddenArtsGame.code.states
 							Settings.keyCastSpell2 = Keyboard.GetState().GetPressedKeys()[0];
 							break;
 						case BindSelected.Inventory:
+                            Settings.keyInventory = Keyboard.GetState().GetPressedKeys()[0];
 							break;
-					}
+                        }
 					binding = false;
 					return false;
 				}
@@ -119,45 +124,77 @@ namespace ForbiddenArtsGame.code.states
 			MouseLoc = new Point(Mouse.GetState().X, Mouse.GetState().Y);
 			if (Mainrect.Contains(MouseLoc) && Mouse.GetState().LeftButton == ButtonState.Pressed)
 			{
-				if(!NeedMouseLift)
+                if ((!NeedMouseLift) || Keyboard.GetState().IsKeyDown(Keys.Enter))
 					return true;
 			}
 			else
 				NeedMouseLift = false;
-			if (FullScreenArea.Contains(MouseLoc))
-			{
-				if (Mouse.GetState().LeftButton == ButtonState.Pressed)
-				{
-					Settings.Fullscreen = !Settings.Fullscreen;
-				}
-			}
-			else if (MusicVolRect.Contains(MouseLoc))
+            if (selected == 11 && Keyboard.GetState().IsKeyDown(Keys.Enter)) {
+                NeedMouseLift = false;
+                return true;
+            }
+			if (MusicVolRect.Contains(MouseLoc) || selected == 0)
 			{
 				if (Mouse.GetState().LeftButton == ButtonState.Pressed)
 				{
 					Settings.MusicVolume = (MouseLoc.X - MusicVolRect.Left) / (float)MusicVolRect.Width;
 				}
+                else if (selected == 0 && Keyboard.GetState().IsKeyDown(Keys.Left)) {
+                    Settings.MusicVolume -= 0.05f;
+                }
+                else if (selected == 0 && Keyboard.GetState().IsKeyDown(Keys.Right)) {
+                    Settings.MusicVolume += 0.05f;
+                }
+                if (Settings.MusicVolume < 0.0f) Settings.MusicVolume = 0;
+                if (Settings.MusicVolume > 1.0f) Settings.MusicVolume = 1.0f;
 			}
-			else if (EffectVolRect.Contains(MouseLoc))
+            if (EffectVolRect.Contains(MouseLoc) || selected == 1)
 			{
 				if (Mouse.GetState().LeftButton == ButtonState.Pressed)
 				{
 					Settings.EffectsVolume = (MouseLoc.X - EffectVolRect.Left) / (float)EffectVolRect.Width;
-				}
+                }
+                else if (selected == 1 && Keyboard.GetState().IsKeyDown(Keys.Left)) {
+                    Settings.EffectsVolume -= 0.05f;
+                }
+                else if (selected == 1 && Keyboard.GetState().IsKeyDown(Keys.Right)) {
+                    Settings.EffectsVolume += 0.05f;
+                }
+                if (Settings.EffectsVolume < 0.0f) Settings.EffectsVolume = 0;
+                if (Settings.EffectsVolume > 1.0f) Settings.EffectsVolume = 1.0f;
 			}
-			else if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+            if (FullScreenArea.Contains(MouseLoc) && Mouse.GetState().LeftButton == ButtonState.Pressed)
+			{
+					Settings.Fullscreen = !Settings.Fullscreen;
+            }
+            if (selected == 2 && Keyboard.GetState().IsKeyDown(Keys.Enter) && oldState.IsKeyUp(Keys.Enter)) {
+                Settings.Fullscreen = !Settings.Fullscreen;
+            }
+			if (Mouse.GetState().LeftButton == ButtonState.Pressed)
 			{
 				for (int iii = 0; iii < KeybindAreas.Length; iii++)
 				{
 					Rectangle r = KeybindAreas[iii];
-					if (r.Contains(MouseLoc))
+                    if (r.Contains(MouseLoc) || selected == iii - 2)
 					{
 						binding = true;
 						selectedBind = (BindSelected)iii;
 						break;
 					}
 				}
-			}
+            }
+            else if (selected > 2 && selected != 11 && Keyboard.GetState().IsKeyDown(Keys.Enter) && oldState.IsKeyUp(Keys.Enter)) {
+                        binding = true;
+                        selectedBind = (BindSelected)(selected-3);
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Up) && binding == false && oldState.IsKeyUp(Keys.Up)) {
+                selected--;
+                if (selected < 0) selected = numOptions - 1;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Down) && binding == false && oldState.IsKeyUp(Keys.Down)) {
+                selected = (selected + 1) % numOptions;
+            }
+            oldState = Keyboard.GetState();
 			return false;
 		}
 
@@ -165,16 +202,16 @@ namespace ForbiddenArtsGame.code.states
 		{
 			Settings.spriteBatch.Draw(sheet, Vector2.Zero, Color.White);//background
 			Settings.spriteBatch.Draw(MusicVolumeSheet, new Vector2(303, 216), new Rectangle(0, 0, (int)(320 * Settings.MusicVolume), 129), Color.White);//music bar
-			if(MusicVolRect.Contains(MouseLoc))
+			if(MusicVolRect.Contains(MouseLoc) || selected == 0)
 			{
 				Settings.spriteBatch.Draw(MusicTitle, new Vector2(301, 217), Color.White);
 			}
 			Settings.spriteBatch.Draw(EffectsVolumeSheet, new Vector2(301, 351), new Rectangle(0, 0, (int)(322 * Settings.EffectsVolume), 167), Color.White);//effects bar
-			if (EffectVolRect.Contains(MouseLoc))
+			if (EffectVolRect.Contains(MouseLoc) || selected == 1)
 			{
 				Settings.spriteBatch.Draw(EffectsTitle, new Vector2(308, 461), Color.White);
 			}
-			Settings.spriteBatch.Draw(FullscreenCheckbox[FullScreenArea.Contains(MouseLoc) ? 1 : 0, Settings.Fullscreen ? 1 : 0], FullScreenArea, Color.White);
+			Settings.spriteBatch.Draw(FullscreenCheckbox[FullScreenArea.Contains(MouseLoc) || selected == 2? 1 : 0, Settings.Fullscreen ? 1 : 0], FullScreenArea, Color.White);
 			for (int iii = 0; iii < KeybindAreas.GetLength(0); iii++)
 			{
 				Settings.spriteBatch.Draw(KeybindTexts[iii, KeybindAreas[iii].Contains(MouseLoc) ? 1 : 0], KeybindAreas[iii], Color.White);
@@ -184,50 +221,50 @@ namespace ForbiddenArtsGame.code.states
 
 			}
 			else
-				Settings.spriteBatch.DrawString(font, Enum.Format(typeof(Keys), Settings.keyMoveLeft, "f"), new Vector2(830, 173), KeybindAreas[0].Contains(MouseLoc) ? Color.Violet : Color.Black);
+				Settings.spriteBatch.DrawString(font, Enum.Format(typeof(Keys), Settings.keyMoveLeft, "f"), new Vector2(830, 173), KeybindAreas[0].Contains(MouseLoc) || selected == 3? Color.Violet : Color.Black);
 			if (binding && selectedBind == BindSelected.Right)
 			{
 
 			}
 			else
-				Settings.spriteBatch.DrawString(font, Enum.Format(typeof(Keys), Settings.keyMoveRight, "f"), new Vector2(830, 198), KeybindAreas[1].Contains(MouseLoc) ? Color.Violet : Color.Black);
+				Settings.spriteBatch.DrawString(font, Enum.Format(typeof(Keys), Settings.keyMoveRight, "f"), new Vector2(830, 198), KeybindAreas[1].Contains(MouseLoc) || selected == 4 ? Color.Violet : Color.Black);
 			if (binding && selectedBind == BindSelected.Jump)
 			{
 
 			}
 			else
-				Settings.spriteBatch.DrawString(font, Enum.Format(typeof(Keys), Settings.keyJump, "f"), new Vector2(830, 223), KeybindAreas[2].Contains(MouseLoc) ? Color.Violet : Color.Black);
+				Settings.spriteBatch.DrawString(font, Enum.Format(typeof(Keys), Settings.keyJump, "f"), new Vector2(830, 223), KeybindAreas[2].Contains(MouseLoc) || selected == 5 ? Color.Violet : Color.Black);
 			if (binding && selectedBind == BindSelected.Interact)
 			{
 
 			}
 			else
-				Settings.spriteBatch.DrawString(font, Enum.Format(typeof(Keys), Settings.keyInteract, "f"), new Vector2(830, 246), KeybindAreas[3].Contains(MouseLoc) ? Color.Violet : Color.Black);
+				Settings.spriteBatch.DrawString(font, Enum.Format(typeof(Keys), Settings.keyInteract, "f"), new Vector2(830, 246), KeybindAreas[3].Contains(MouseLoc) || selected == 6 ? Color.Violet : Color.Black);
 			if (binding && selectedBind == BindSelected.Melee)
 			{
 
 			}
 			else
-				Settings.spriteBatch.DrawString(font, Enum.Format(typeof(Keys), Settings.keyMeleeAttack, "f"), new Vector2(830, 269), KeybindAreas[4].Contains(MouseLoc) ? Color.Violet : Color.Black);
+				Settings.spriteBatch.DrawString(font, Enum.Format(typeof(Keys), Settings.keyMeleeAttack, "f"), new Vector2(830, 269), KeybindAreas[4].Contains(MouseLoc) || selected == 7 ? Color.Violet : Color.Black);
 			if (binding && selectedBind == BindSelected.Spell1)
 			{
 
 			}
 			else
-				Settings.spriteBatch.DrawString(font, Enum.Format(typeof(Keys), Settings.keyCastSpell1, "f"), new Vector2(830, 293), KeybindAreas[5].Contains(MouseLoc) ? Color.Violet : Color.Black);
+				Settings.spriteBatch.DrawString(font, Enum.Format(typeof(Keys), Settings.keyCastSpell1, "f"), new Vector2(830, 293), KeybindAreas[5].Contains(MouseLoc) || selected == 8 ? Color.Violet : Color.Black);
 			if (binding && selectedBind == BindSelected.Spell2)
 			{
 
 			}
 			else
-				Settings.spriteBatch.DrawString(font, Enum.Format(typeof(Keys), Settings.keyCastSpell2, "f"), new Vector2(830, 319), KeybindAreas[6].Contains(MouseLoc) ? Color.Violet : Color.Black);
+				Settings.spriteBatch.DrawString(font, Enum.Format(typeof(Keys), Settings.keyCastSpell2, "f"), new Vector2(830, 319), KeybindAreas[6].Contains(MouseLoc) || selected == 9 ? Color.Violet : Color.Black);
 			if (binding && selectedBind == BindSelected.Inventory)
 			{
 
 			}
 			else
-				Settings.spriteBatch.DrawString(font, Enum.Format(typeof(Keys), Settings.keyInventory, "f"), new Vector2(830, 343), KeybindAreas[7].Contains(MouseLoc) ? Color.Violet : Color.Black);
-			if (Mainrect.Contains(MouseLoc))
+				Settings.spriteBatch.DrawString(font, Enum.Format(typeof(Keys), Settings.keyInventory, "f"), new Vector2(830, 343), KeybindAreas[7].Contains(MouseLoc) || selected == 10 ? Color.Violet : Color.Black);
+			if (Mainrect.Contains(MouseLoc) || selected == 11)
 				Settings.spriteBatch.Draw(Mainmenu, Mainrect, Color.White);
 		}
 	}
